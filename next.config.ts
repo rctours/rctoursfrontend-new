cc9fb6de97383f4fc4e4fe716386e2031373a7e8
unknown <rupesh.rctoursandtravels@gmail.com>
2026-08-21 16:42:51 +0530

    Add redirects for old website URLs

--- next.config.ts
+++ next.config.ts
@@ -15,6 +15,29 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+
+  async redirects() {
+    return [
+      // OLD WEBSITE - Available Cars
+      {
+        source: "/cars",
+        destination: "/fleet",
+        permanent: true,
+      },
+
+      // OLD WEBSITE - old blog URLs found in Google
+      {
+        source: "/blog/n4udcjmgcl9mjgnljgt6day0",
+        destination: "/blog",
+        permanent: true,
+      },
+      {
+        source: "/blog/qgpsgh41zz6jer4og1ywxtu9",
+        destination: "/blog",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
